49adbe1aeba69bd6af6c6df2f5f256e947693205
Philipp Ludewig <diesdas@philippludewig.de>
2023-10-25 15:01:39 +0200

    Change method name to reflect purpose

--- src/main/kotlin/banking/domain/Customer.kt
+++ src/main/kotlin/banking/domain/Customer.kt
@@ -2,7 +2,7 @@ package banking.domain
 
 data class Customer(val name: String, val bankAccounts: List<BankAccount>) {
 
-    fun updateAddress(address: Address): Customer{
+    fun updateAddressOfAllBankAccounts(address: Address): Customer{
         return Customer(name, bankAccounts.map{ it.copy(address = address) } )
     }
 }
--- src/test/kotlin/banking/domain/CustomerTest.kt
+++ src/test/kotlin/banking/domain/CustomerTest.kt
@@ -3,7 +3,6 @@ package banking.domain
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 import java.util.*
-import kotlin.test.currentStackTrace
 
 class CustomerTest{
 
@@ -11,7 +10,7 @@ class CustomerTest{
     fun `should update all BankAccount connected Addresses`() {
         val customer = Customer("Nelly Bly", listOf(BankAccount(UUID.randomUUID(),Address("Berlin", "Einsteinstr 4"))))
 
-        val updateAddress = customer.updateAddress(Address("Mumbai", "somewhere"))
+        val updateAddress = customer.updateAddressOfAllBankAccounts(Address("Mumbai", "somewhere"))
 
         assertEquals("Mumbai", updateAddress.bankAccounts.get(0).address.city)
     }
